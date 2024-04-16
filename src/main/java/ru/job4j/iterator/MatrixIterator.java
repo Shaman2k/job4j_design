94426535f06cedab1c230b8fb93c3c78bba003f6
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2024-04-16 13:18:06 +0300

    2. Итератор для двухмерного массива int[][] [#9539]

---
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,37 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+    private final int[][] data;
+    private int row;
+    private int column;
+
+    public MatrixIterator(int[][] data) {
+        this.data = data;
+        row = 0;
+        column = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (row < data.length) {
+            if (column >= data[row].length) {
+                column = 0;
+                row++;
+            } else {
+                break;
+            }
+        }
+        return row < data.length && column < data[row].length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+}
