e4428a5056484560e63d3846837eb12370b0549d
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2024-05-12 18:53:33 +0300

    2.0. FileOutputStream [#252488]

--- src/test/java/ru/job4j/question/AnalizeTest.java
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -2,11 +2,8 @@ package ru.job4j.question;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import java.util.Set;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 class AnalizeTest {
 
