6de53ed6a7a53a109bc5a0bcda225db17be03f1b
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2024-05-15 10:51:07 +0300

    10. Сканирование файловой системы [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+    private final Predicate<Path> conditions;
+    private final List<Path> paths = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> conditions) {
+        this.conditions = conditions;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attributes) {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+        if (conditions.test(file)) {
+            paths.add(file);
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exception) {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path directory, IOException exception) {
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+}
