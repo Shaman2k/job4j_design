680b2809a0654eb08879bed8b3b25a36ce85da98
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2024-05-16 11:49:39 +0300

    11. Поиск дубликатов [#315066 #496267]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,7 +1,6 @@
 package ru.job4j.io.duplicates;
 
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -12,10 +11,8 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
-        if (Files.isRegularFile(file)) {
-            FileProperty fileProperty = new FileProperty(attributes.size(), file.getFileName().toString());
-            filenames.computeIfAbsent(fileProperty, k -> new HashSet<>()).add(file);
-        }
+        FileProperty fileProperty = new FileProperty(attributes.size(), file.getFileName().toString());
+        filenames.computeIfAbsent(fileProperty, k -> new HashSet<>()).add(file);
         return FileVisitResult.CONTINUE;
     }
 
