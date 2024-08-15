938772aab7e5c0472418e6f7137f9af6f79ef589
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2024-08-15 13:31:52 +0300

    0.2. PreparedStatement [#379307]

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,74 @@
+package ru.job4j.spammer;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private final Properties config;
+    private final String dump;
+
+    public ImportDB(Properties config, String dump) {
+        this.config = config;
+        this.dump = dump;
+    }
+
+    private User parse(String string) {
+        String[] data = string.split(";");
+        if (data.length != 2 || data[0].isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return new User(data[0], data[1]);
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(dump))) {
+            reader.lines().forEach(s -> users.add(parse(s)));
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(config.getProperty("jdbc.driver"));
+        try (Connection connection = DriverManager.getConnection(
+                config.getProperty("jdbc.url"),
+                config.getProperty("jdbc.username"),
+                config.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO users(name, email) VALUES (?, ?)")) {
+                    preparedStatement.setString(1, user.name);
+                    preparedStatement.setString(2, user.email);
+                    preparedStatement.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties config = new Properties();
+        try (InputStream input = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(input);
+        }
+        ImportDB dataBase = new ImportDB(config, "./data/dump.txt");
+        dataBase.save(dataBase.load());
+    }
+}
