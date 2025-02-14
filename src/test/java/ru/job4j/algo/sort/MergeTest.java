29d0bd5d0254d6a44c11e5a7af20fe3e3e808e5a
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2025-02-14 15:42:57 +0300

    Сортировка слиянием [#504997]

---
+++ src/test/java/ru/job4j/algo/sort/MergeTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.algo.sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MergeTest {
+    @Test
+    void whenSortedThenOk() {
+        int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
+        assertThat(Merge.mergesort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
+    }
+
+    @Test
+    void whenSortedequalsThenOk() {
+        int[] array = {1, 1};
+        assertThat(Merge.mergesort(array)).containsExactly(1, 1);
+    }
+
+    @Test
+    void whenSortedOneElementThenOk() {
+        int[] array = {1};
+        assertThat(Merge.mergesort(array)).containsExactly(1);
+    }
+}
